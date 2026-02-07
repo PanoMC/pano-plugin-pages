c3cef778991627a45e85c097abec5de5deee6bad
kahverengi <ahmet@duruer.dev>
2026-02-07 21:04:25 +0300

    fix: fixed enable disable errors

--- src/main/kotlin/com/panomc/plugins/pages/PagesPlugin.kt
+++ src/main/kotlin/com/panomc/plugins/pages/PagesPlugin.kt
@@ -18,11 +18,6 @@ class PagesPlugin : PanoPlugin() {
     override suspend fun onStart() {
         logger.info("Starting...")
 
-        if (!setupManager.isSetupDone()) {
-            logger.info("Setup is not finished, waiting for setup completion...")
-            return
-        }
-
         startPlugin()
     }
 
@@ -30,7 +25,18 @@ class PagesPlugin : PanoPlugin() {
         if (isInitialized) return
         isInitialized = true
 
+        if (!setupManager.isSetupDone()) {
+            logger.info("Setup is not finished, waiting for setup completion...")
+            return
+        }
+
         pluginDatabaseManager.initialize(this)
+    }
+
+    override suspend fun onEnable() {
+        logger.info("Enabled!")
+
+        startPlugin()
     }
 
     override suspend fun onUninstall() {
