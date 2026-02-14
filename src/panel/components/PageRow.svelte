713d0c213ae319dd708e6b22a90651a540b32bdc
Selim Gökçek <selimgokcek@outlook.com>
2026-02-14 23:30:44 +0300

    feat: Added tooltip indicators on the panel page

--- src/panel/components/PageRow.svelte
+++ src/panel/components/PageRow.svelte
@@ -7,7 +7,7 @@
         aria-expanded="false"
         aria-haspopup="true"
         data-bs-toggle="dropdown"
-        title={$_('pages.list.table.actions')}
+        use:tooltip={[$_('pages.list.table.actions')]}
         aria-label={$_('pages.list.table.actions')}>
         <span class="fas fa-ellipsis-v"></span>
       </button>
@@ -32,6 +32,7 @@
     <button
       type="button"
       on:click={() => onEditClick(page.id)}
+      use:tooltip={[$_('pages.list.table.actions-edit')]}
       title={page.title}
       aria-label={page.title}
       class="btn btn-link p-0 text-start text-decoration-none w-100 text-truncate focus-ring">
@@ -55,6 +56,7 @@
 
 <script>
   import { _ } from '../../main';
+  import tooltip from '@panomc/sdk/utils/tooltip';
 
   export let page;
   export let onEditClick;
